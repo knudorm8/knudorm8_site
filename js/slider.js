a388b7ff6359c75ec9e314d419b8bc46c9bc7159
Denis <den467193@gmail.com>
2024-08-23 23:08:33 +0300

    The slider has been updated. Margins for images need to be changed (Chrome).

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(function() {
-    document.querySelectorAll('.swiperContainer').forEach((container, index) => {
-      const swiper = new Swiper(container.querySelector('.newSwiper'), {
-        navigation: {
-          nextEl: container.querySelector('.next-btn'),
-          prevEl: container.querySelector('.prev-btn'),
-        },
-        pagination: {
-          el: container.querySelector('.swiper-pagination'),
-          clickable: true,
-        },
-      });
-    });
-  }, 1000);
+  function handleHashChange() {
+    if (window.location.hash === '#guides/how-to-pay') {
+      setTimeout(function() {
+        document.querySelectorAll('.swiperContainer').forEach((container, index) => {
+          const swiper = new Swiper(container.querySelector('.newSwiper'), {
+            navigation: {
+              nextEl: container.querySelector('.next-btn'),
+              prevEl: container.querySelector('.prev-btn'),
+            },
+            pagination: {
+              el: container.querySelector('.swiper-pagination'),
+              clickable: true,
+            },
+          });
+        });
+      }, 1000);
+    }
+  }
+
+  window.addEventListener('hashchange', handleHashChange);
+
+  handleHashChange();
 });
